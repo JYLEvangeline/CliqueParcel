317abe0c5e438744750a92bd2fdfc01e0e89e326
JYLEvangeline <33863813+JYLEvangeline@users.noreply.github.com>
2024-02-09 02:30:05 -0500

    Update generate_dot_run_file.py

--- generate_dot_run_file.py
+++ generate_dot_run_file.py
@@ -7,7 +7,7 @@ def parse_arguments():
     parser.add_argument("--length_num", type = int, default = 256)
     parser.add_argument("--dataset", default = "hotpot_qa", choices = ["trec","squad","hotpot_qa"])
     parser.add_argument("--model", default = "gpt-3.5-turbo", choices = ["gpt-3.5-turbo", "gpt-4"])
-    parser.add_argument("--mode", default = "random_plus_avg_length", choices = ["group", "sequence", "random", "full_random", "semantic_sim", "concept_plus_semantic_sim", "avg_length", "seq_length", "maximum_diff", 'random_plus_avg_length'])
+    parser.add_argument("--mode", default = "random_plus_avg_length", choices = ["group", "full_random", "semantic_sim", "concept_plus_semantic_sim", "avg_length", "seq_length", "maximum_diff", 'random_plus_avg_length'])
     parser.add_argument("--i_th_experiment", type = str, default = "1.query_length")
     parser.add_argument("--resume", type = bool, default = True)
     args = parser.parse_args()
@@ -15,7 +15,7 @@ def parse_arguments():
 
 datasets = ["hotpot_qa"]
 models = ["gpt-3.5-turbo", "gpt-4"]
-modes = ["group", "seperate", "sequence", "full_random", "semantic_sim", "concept_plus_semantic_sim", "avg_length", "seq_length", "maximum_diff", 'random_plus_avg_length']
+modes = ["group", "seperate", "full_random", "semantic_sim", "concept_plus_semantic_sim", "avg_length", "seq_length", "maximum_diff", 'random_plus_avg_length']
 for dataset in datasets:
     for model in models:
         for mode in modes:
